a6ec83d7e5f60bafc95ab5115582148169fb2285
David G. Simmons <santafen@mac.com>
2016-12-13 16:12:53 -0500

    Initial Commit

---
+++ MyNewt Sensors/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyNewt Sensors
+//
+//  Created by David G. Simmons on 12/13/16.
+//  Copyright © 2016 Dragonfly IoT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyNewt Sensors/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyNewt Sensors
+//
+//  Created by David G. Simmons on 12/13/16.
+//  Copyright © 2016 Dragonfly IoT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
